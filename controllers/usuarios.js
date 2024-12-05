5c7367703a17857b8472163e6fcef3520a81e342
JuanFachas <juanes.orteguita.2006@gmail.com>
2024-12-04 23:30:18 -0500

    Implementada y aprobada la funcionalidad para inhabilitar usuarios

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -69,6 +69,20 @@ ruta.put ('/:email', (req, res) => {
     }
 });
 
+//Endpoint de tipo DELETE para el recurso USUARIOS
+ruta.delete('/:email', (req, res) => {
+    let resultado = desactivarUsuario(req.params.email);
+    resultado.then(valor => {
+        res.json({
+            usuario: valor
+        })
+    }).catch(err => {
+        res.status(400).json({
+            err
+        })
+    });
+});
+
 async function actualizarUsuario(email, body){
     let usuario = await Usuario.findOneAndUpdate({"email": email}, {
         $set: {
@@ -87,4 +101,14 @@ async function crearUsuario(body){
         password    :body.password
     });
     return await usuario.save();
+}
+
+//Funcion asincrona para inactivar un usuario
+async function desactivarUsuario(email){
+    let usuario = await Usuario.findOneAndUpdate({"email": email}, {
+        $set: {
+            estado: false
+        }
+    }, {new: true});
+    return usuario;
 }
